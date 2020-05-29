e3fb6f15b4bb8c902a49847735d79f82ec00b789
Shaiene-kun <rafaelshaiene115@gmail.com>
2020-05-28 22:18:26 -0300

    Automated Installation of flatpak apps (Forgot the -y))
    Commentary correction
    
    Adding Flathub repositories

--- flatpak/flatpak-installer.sh
+++ flatpak/flatpak-installer.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
 #Installing flatpak
-
 if (( $(apt list --installed flatpak | grep -c flatpak) < 1 ))
 then
    apt-get install -y flatpak 
 fi
 
-appList=$(cat ./flatpak-apps.txt) #Obtaining apps/packages list from txt file
+#Adding FlatHub repositories
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
+
+#Obtaining apps/packages list from txt file
+appList=$(cat ./flatpak-apps.txt) 
 echo "Pacotes a serem instalados $appList"
 
 #Instalando 
 for app in $appList
 do
-   flatpak install flathub $app
+   flatpak install -y flathub $app
 done
