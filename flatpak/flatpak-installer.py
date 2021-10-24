c73b7cff59c8343d8cae400ce31e83c262d2e5a6
ShaieneKun <rafaelshaiene115@gmail.com>
2021-10-24 04:40:16 -0300

    Fixing bug

--- flatpak/flatpak-installer.py
+++ flatpak/flatpak-installer.py
@@ -3,6 +3,7 @@ import subprocess
 
 with open("flatpak-apps.txt", "r") as flatpak_apps_file:
     flatpak_apps: 'list[str]' = flatpak_apps_file.readlines()
+    flatpak_apps = [ app.replace("\n", "") for app in flatpak_apps ]
 
 if not flatpak_apps:
     print(f"Flatpaks a serem instalados: {flatpak_apps}")
--- snap/snap-installer.py
+++ snap/snap-installer.py
@@ -2,6 +2,7 @@ import subprocess
 
 with open("snap-apps.txt", "r") as snap_apps_file:
     snap_apps: 'list[str]' = snap_apps_file.readlines()
+    snap_apps = [ app.replace("\n", "") for app in snap_apps ]
 
 if not snap_apps:
     print(f"snaps a serem instalados: {snap_apps}")
