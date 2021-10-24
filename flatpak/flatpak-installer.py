e45ea632b042517cda95b4c18a7536d5fbbaf8d3
ShaieneKun <rafaelshaiene115@gmail.com>
2021-10-24 04:43:54 -0300

    Fixing bug

--- flatpak/flatpak-installer.py
+++ flatpak/flatpak-installer.py
@@ -5,7 +5,7 @@ with open("flatpak-apps.txt", "r") as flatpak_apps_file:
     flatpak_apps: 'list[str]' = flatpak_apps_file.readlines()
     flatpak_apps = [ app.replace("\n", "") for app in flatpak_apps ]
 
-if not flatpak_apps:
+if flatpak_apps:
     print(f"Flatpaks a serem instalados: {flatpak_apps}")
     
     for app in flatpak_apps:
--- snap/snap-installer.py
+++ snap/snap-installer.py
@@ -4,7 +4,7 @@ with open("snap-apps.txt", "r") as snap_apps_file:
     snap_apps: 'list[str]' = snap_apps_file.readlines()
     snap_apps = [ app.replace("\n", "") for app in snap_apps ]
 
-if not snap_apps:
+if snap_apps:
     print(f"snaps a serem instalados: {snap_apps}")
     
     for app in snap_apps:
